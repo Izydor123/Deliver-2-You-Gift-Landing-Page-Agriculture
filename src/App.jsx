cfe3c0eb4e87c4704d08b60ba743cf2f292bac5d
Izydor123 <djqyubi@gmail.com>
2025-04-08 13:33:03 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,16 @@ function App() {
       <Header title="Agricultura" desc1="Oferecemos produtos agrícolas que simplificam sua rotina e aumentam sua produtividade." desc2="Encontre soluções modernas para cuidar da terra com eficiência e sustentabilidade"/>
       <Product
        image={imageMain} 
-       title="Navaja Multiusos Texwol: A Versatilidade num Objecto Compacto" 
+       title="Canivete Multiusos Texwol: A Versatilidade num Objecto Compacto" 
        desc1="Este canivete multiusos é um exemplo clássico da engenhosidade suíça. Com o seu característico cabo vermelho e múltiplas ferramentas em aço inoxidável, representa mais do que um simples instrumento de corte - é um verdadeiro companheiro para aventureiros, campistas e entusiastas do outdoor."
        desc2="As suas diversas funcionalidades, que incluem lâminas de diferentes tamanhos, tesouras, saca-rolhas e destorniladores, tornam-no numa ferramenta extremamente útil para inúmeras situações. Desde abrir uma garrafa de vinho durante um piquenique até reparar um equipamento durante uma caminhada, este canivete resolve praticamente tudo."
        link="https://generalcatalogue2025.eu/deliver2yougift/#page/846"/>
       <ProductsList 
         data={[
-        {id:1, text:"Llavero Navaja Koburn", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/847", image:image1},
-        {id:2, text:"Lupa Llavero Kitins 8X", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/848", image:image2},
-        {id:3, text:"Combitester Pyrom", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/852", image:image3},
-        {id:4, text:"Estación Meteorológica Geibol", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/854", image:image4},
+        {id:1, text:"Porta-Chaves Canivete Koburn", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/847", image:image1},
+        {id:2, text:"Lupa Porta-Chaves Kitins 8X", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/848", image:image2},
+        {id:3, text:"Higrometro de Solo Pyrom", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/852", image:image3},
+        {id:4, text:"Estacao Meteorológica Geibol", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/854", image:image4},
         ]}
         title="Outros produtos desta categoria"/>
       <Catalogue text="Conheça os nossos outros produtos associados à agricultura" link="https://generalcatalogue2025.eu/deliver2yougift/#page/844"/>
